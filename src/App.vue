9db8d9356ef336b412468319ba42514a75b644e4
archandy85 <80558340+archandy85@users.noreply.github.com>
2021-06-24 22:29:51 +0900

    vuetest

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,42 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+    >
+     <v-list-item router :to="{name:'Home'}">
+        <v-list-item-icon>
+          <v-icon>mdi-inbox-arrow-down</v-icon>
+        </v-list-item-icon>
+
+        <v-list-item-content>
+          <v-list-item-title>Home</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+      <v-list-item router :to="{name:'Login'}">
+        <v-list-item-icon>
+          <v-icon>mdi-send</v-icon>
+        </v-list-item-icon>
+
+        <v-list-item-content>
+          <v-list-item-title>Login</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-navigation-drawer>
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default {
+  data: () => ({ drawer: null })
 }
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
---
+++ src/views/Login.vue
@@ -0,0 +1,36 @@
+<template>
+    <v-container fill-height style="max-width:400px">
+        <v-layout align-center row rwap>
+            <v-flex xs12>
+            <v-card>
+              <v-toolbar flat height="30" class="pa-3">
+                <v-toolbar-title>Login</v-toolbar-title>
+                <v-spacer></v-spacer>
+              </v-toolbar>
+              <div class="pa-3">
+              <v-text-field
+                  v-model="email"
+                  label="email@sample.com"
+          ></v-text-field>
+          <v-text-field
+                  v-model="password"
+                  type="password"
+                  label="password"
+          ></v-text-field>
+              </div>
+          </v-card>
+          </v-flex>
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+export default ({
+  data () {
+    return {
+      email: null,
+      password: null
+    }
+  }
+})
+</script>
